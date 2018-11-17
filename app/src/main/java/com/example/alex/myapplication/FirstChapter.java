7ab342b013e73f9d13987fc88a7a8ee04a43406c
Alex <skainrusak@gmail.com>
2018-11-18 01:43:42 +0600

    Two chapter

--- app/src/main/java/com/example/alex/myapplication/FirstChapter.java
+++ app/src/main/java/com/example/alex/myapplication/FirstChapter.java
@@ -4,24 +4,68 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FirstChapter extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question [] mQuestionsBank = new Question[]{        // Вызов конструктор  Question
+            new Question(R.string.question_oceans ,true),
+            new Question(R.string.question_mideast , false),
+            new Question(R.string.question_africa , false),
+            new Question(R.string.question_americas , true),
+            new Question(R.string.question_asia , true),
+    };
+            private int mCurrentIndex = 0 ;
+
+            private void updateQuestion (){
+                int question = mQuestionsBank [mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_chapter);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex +1) % mQuestionsBank.length;
+                int question = mQuestionsBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+        updateQuestion();
     }
+
+    private void checkAnswer (boolean userPressedTrue){
+                boolean answerIsTrue = mQuestionsBank[mCurrentIndex].isAnswerTrue();
+                int messageResId = 0;
+
+                if (userPressedTrue == answerIsTrue){
+                    messageResId = R.string.correct_toast;
+                }
+                else {
+                    messageResId = R.string.incorrect_toast;
+                }
+                Toast.makeText(this,messageResId ,Toast.LENGTH_SHORT).show();
+    }
+
     public void True (View view){
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(FirstChapter.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
     }
@@ -30,7 +74,7 @@ public class FirstChapter extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstChapter.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
     }
