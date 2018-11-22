9d858b62192e7c06dcc9ede273a84a66dc5b35ec
Alex <skainrusak@gmail.com>
2018-11-22 13:34:01 +0600

    Sixth lesson without assignment

--- app/src/main/java/com/example/alex/myapplication/CheatActivity.java
+++ app/src/main/java/com/example/alex/myapplication/CheatActivity.java
@@ -1,16 +1,20 @@
 package com.example.alex.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.alex.myapplication.answer_is_true";
-    public static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+    public static final String EXTRA_ANSWER_SHOWN = "com.example.android.myapplication.answer_shown";
     public static Intent newIntent(Context packageContext , boolean answerIsTrue){
         Intent intent = new Intent(packageContext , CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE , answerIsTrue);
@@ -34,13 +38,30 @@ private boolean mAnswerIsTrue ;
          mShowAnswer.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 if (mAnswerIsTrue){
+                 if (mAnswerIsTrue) {
                      mAnswerTextView.setText(R.string.true_button);
-                 }
-                 else {
+                 } else {
                      mAnswerTextView.setText(R.string.false_button);
                  }
                  setAnswerShownResult(true);
+                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                     int cx = mShowAnswer.getWidth() / 2;
+                     int cy = mShowAnswer.getHeight() / 2;
+                     float radius = mShowAnswer.getWidth();
+                     Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                     anim.addListener(new AnimatorListenerAdapter() {
+                         @Override
+                         public void onAnimationEnd(Animator animation) {
+                             super.onAnimationEnd(animation);
+                             mAnswerTextView.setVisibility(View.VISIBLE);
+                             mShowAnswer.setVisibility(View.INVISIBLE);
+                         }
+                     });
+                     anim.start();
+                 } else {
+                     mAnswerTextView.setVisibility(View.VISIBLE);
+                     mShowAnswer.setVisibility(View.INVISIBLE);
+                 }
              }
          });
     }
