64ff405383ce9109baef346e01325e4ff352b2af
alex <skainrusak@gmail.com>
2018-11-26 19:36:08 +0600

    eighth chapter

--- app/src/main/java/com/example/alex/myapplication/Crime.java
+++ app/src/main/java/com/example/alex/myapplication/Crime.java
@@ -1,12 +1,33 @@
 package com.example.alex.myapplication;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
     public Crime (){
         mId = UUID.randomUUID();
+        mDate = new Date();
 
 
     }
--- app/src/main/java/com/example/alex/myapplication/CrimeFragment.java
+++ app/src/main/java/com/example/alex/myapplication/CrimeFragment.java
@@ -7,11 +7,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.text.format.DateFormat;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime ;
     private EditText mTitleField;
+    private Button mDateButton ;
+    private CheckBox mSolvedCheckBox ;
 
     @Override
     public void onCreate(Bundle savedInstance){
@@ -38,6 +44,20 @@ mCrime.setTitle(c.toString());
 
             }
         });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        String data = DateFormat.format("EEEE , MMM d , yyyy" ,mCrime.getmDate()).toString();
+        mDateButton.setText(data);
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);          //Чек бокс для проверки раскрыто ли преступление
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setmSolved(isChecked);
+            }
+        });
+
         return v;
     }
 
